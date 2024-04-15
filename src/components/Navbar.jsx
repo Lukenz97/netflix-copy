8335d5fa936320db30db381b1b0e8e08e7c5c296
lukaZgonjanin <luka.zgonjanin@devoteam.com>
2024-04-15 19:58:59 +0200

    Inital commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>Netflix</h1>
+      <div>
+        <button>Sign In</button>
+        <button>Sign Up</button>
+
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
+ 
